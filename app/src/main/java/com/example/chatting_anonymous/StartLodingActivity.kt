707763766f012b2c2f073db49ab4948d278af637
ParkSangSun1 <appleasz1028102@gmail.com>
2020-12-22 14:07:31 +0900

    디자인 변경전

---
+++ app/src/main/java/com/example/chatting_anonymous/StartLodingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chatting_anonymous
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartLodingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_loding)
+    }
+}
